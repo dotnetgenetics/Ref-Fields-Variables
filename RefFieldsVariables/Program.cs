262ef90955dbf3d69db16d0b22c053f9bc776f39
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-19 17:32:26 +0800

    Add project files.

---
+++ RefFieldsVariables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RefFieldsVariables
+{
+    ref struct Wine
+    {
+        public ref int AgeInYears;
+
+        public Wine(ref int age)
+        {
+            AgeInYears = ref age;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+
+            Wine wine = new Wine(ref age);
+            ref int localAge = ref wine.AgeInYears;
+
+            Console.WriteLine($"Age of wine is {localAge}");
+
+            Console.ReadLine();
+        }
+    }
+}
